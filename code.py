e54d280c29963bead93e66e929fbb81b5449be95
Kent Gatera <kent.gatera@stu.ocsb.ca>
2023-01-09 19:27:31 +0000

    This program is the 'Space Aliens' program on the pyBadge.

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Created by: Kent Gatera
+# Created on: Jan 9th
+# This program is the "Space Aliens" program on the pyBadge.
+
+# import ugame
+# import stage
+
+
+def game_scene():
+    # Prints 3 new blank lines.
+    print("\n\n\n")
+    # Prints Hello world
+    print("Hello, World!")
+    # Place holder.
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    game_scene()
